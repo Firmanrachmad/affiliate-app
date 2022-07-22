1c358aafa838125650e33da015f56b7b046f12a4
Firmanrachmad <dragobniel@gmail.com>
2022-07-22 20:28:27 +0700

    testing template

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/frontend', function () {
+    return view('layouts.template');
+});
+
+Route::get('/backend', function () {
+    return view('layouts.template1');
+});
